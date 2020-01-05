58ae5587ea4b6e0897db7abd462d31e8794fb02e
vitalenacho <59450226+vitalenacho@users.noreply.github.com>
2020-01-05 17:45:25 -0300

    Update geolocation.js

--- geolocation.js
+++ geolocation.js
@@ -11,4 +11,18 @@ function showPosition(position) {
   window.location = 'https://www.google.com/maps/dir/'+position.coords.latitude+','+position.coords.longitude+'/-31.735050,-60.522094'  
   x.innerHTML = "Latitude: " + position.coords.latitude + 
   "<br>Longitude: " + position.coords.longitude;
+
+}
+
+function getConstantLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(showConstantPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showConstantPosition(position){
+    x.innerHTML="Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
 }
