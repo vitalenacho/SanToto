83a6ba0bf1f31823987a1fd2ed962b5e6d7d9a84
vitalenacho <59450226+vitalenacho@users.noreply.github.com>
2020-05-05 03:20:42 -0300

    Update geolocation.js

--- geolocation.js
+++ geolocation.js
@@ -1,28 +1,44 @@
 var x = document.getElementById("demo");
-function getLocation() {
-  if (navigator.geolocation) {
+var destino = document.getElementById("destino");
+function getLocation(data) {
+  if (navigator.geolocation && data != "cliente") {
+    destino.innerHTML = data;
     navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
+  } else if (navigator.geolocation && data =="cliente" ) {
+    navigator.geolocation.getCurrentPosition(showClientPosition);
+  }else {
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
 
 function showPosition(position) {
-  window.location = 'https://www.google.com/maps/dir/'+position.coords.latitude+','+position.coords.longitude+'/-31.735050,-60.522094'  
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
-
-}
-
-function getConstantLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.watchPosition(showConstantPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-
-function showConstantPosition(position){
-    x.innerHTML="Latitude: " + position.coords.latitude + 
+    //window.location = 'https://www.google.com/maps/dir/'+position.coords.latitude+','+position.coords.longitude+'/-31.735050,-60.522094'
+    var page = 'https://www.google.com/maps/dir/'+position.coords.latitude+','+position.coords.longitude+'/'+destino.innerHTML
+    window.open(page, '_blank', 'width=800, height=600 , location=no');
+    //google.script.host.close();
+    
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude;
-}
+  }
+  
+
+  function showClientPosition(position) {
+    clientLat = document.getElementById("clientLat");
+    clientLon = document.getElementById("clientLon");
+    clientLat.value = position.coords.latitude;
+    clientLon.value = position.coords.longitude;
+  }
+  function getConstantLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(showConstantPosition);
+    } else { 
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+  
+  function showConstantPosition(position){
+      x.innerHTML="Latitude: " + position.coords.latitude + 
+      "<br>Longitude: " + position.coords.longitude;
+      var data =[1,position.coords.latitude,position.coords.longitude];
+      google.script.run.insertDirection(data);
+  }
